f02680322a095e6b8afe26ea3c66ae7f2a74d549
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-06-06 00:32:39 +0530

    seventh commit

---
+++ src/queue/ArrayDequeRunner.java
@@ -0,0 +1,7 @@
+package queue;
+
+public class ArrayDequeRunner {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/queue/PriorityQueueRunner.java
@@ -0,0 +1,41 @@
+package queue;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class PriorityQueueRunner {
+    public static void main(String[] args) {
+
+        Queue<String> priorityqueue = new PriorityQueue<>();
+
+        /* these methods throw exceptions: */
+
+        //offer(Object o):
+        priorityqueue.offer("Jay");
+        priorityqueue.offer("Adams");
+        priorityqueue.offer("Mike");
+
+        //peek():
+        System.out.println(priorityqueue.peek());
+
+        //poll():
+        System.out.println(priorityqueue.poll());
+        System.out.println(priorityqueue.poll());
+
+        /* these methods return false/null: */
+
+        //add(Object o)
+        priorityqueue.add("Sammy");
+        priorityqueue.add("Quora");
+
+        priorityqueue.forEach(System.out::println);
+
+        //element():
+        System.out.println(priorityqueue.element());
+
+        //poll():
+        System.out.println(priorityqueue.remove());
+        System.out.println(priorityqueue.remove());
+    }
+}

210cc6bde6c98c52a89894fd5af22740d5fee05e
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-06-06 00:37:57 +0530

    eighth commit

--- src/queue/ArrayDequeRunner.java
+++ src/queue/ArrayDequeRunner.java
@@ -1,7 +1,19 @@
 package queue;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+//import java.util.Queue;
+
 public class ArrayDequeRunner {
     public static void main(String[] args) {
 
+//        Queue<Integer> arraydeque = new ArrayDeque<>();
+        Deque<Integer> arraydeque = new ArrayDeque<>();
+
+        /* these methods throw exceptions: */
+
+
+
+        /* these methods return false/null: */
     }
 }

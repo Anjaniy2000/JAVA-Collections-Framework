3c677435920cbfbdf7eaba25f87c0b5d54f20ac6
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-06-09 16:36:55 +0530

    fourteenth commit

--- src/comparableAndComparator/comparator/MainClass.java
+++ src/comparableAndComparator/comparator/MainClass.java
@@ -2,6 +2,7 @@ package comparableAndComparator.comparator;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class MainClass {
@@ -12,7 +13,7 @@ public class MainClass {
         players.add(new Player("Jay",1, "Cricket"));
         players.add(new Player("Mahesh",4, "Basketball"));
         players.add(new Player("Adams",2, "Baseball"));
-        players.add(new Player("Adams",5, "Running"));
+        players.add(new Player("Adams",5, "Baseball"));
         System.out.println("Before Sorting:");
         players.forEach(System.out::println);
 
@@ -21,8 +22,14 @@ public class MainClass {
 //        Collections.sort(players, new Player.sortByNameThenMedals());
 //        players.forEach(System.out::println);
 
-        System.out.println("Sort By Game Name:");
-        Collections.sort(players, new Player.sortByGameName());
+//        System.out.println("Sort By Game Name:");
+//        Collections.sort(players, new Player.sortByGameName());
+//        players.forEach(System.out::println);
+
+        /* Enhanced Comparator Using Lambda Expression: */
+        //Sort By Name -> Game Name -> No. Of Medals:
+        Collections.sort(players, Comparator.comparing(Player::getName).thenComparing(Player::getGameName).thenComparing(Player::getMedals));
         players.forEach(System.out::println);
+
     }
 }
